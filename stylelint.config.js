13cc398fad18d60f7e95f13a68438b7a2f770052
firefoxic <firefoxic.dev@gmail.com>
2024-08-20 08:07:30 +0300

    Update dependencies

--- stylelint.config.js
+++ stylelint.config.js
@@ -5,15 +5,4 @@ export default {
 		`stylelint-config-html/astro`,
 		`stylelint-config-html/svelte`,
 	],
-	"rules": {
-		"declaration-property-value-no-unknown": [
-			true,
-			{
-				typesSyntax: {
-					color: `| oklch( [ <percentage> | <number> | none] [ <percentage> | <number> | none] [ <hue> | none] [ / [<alpha-value> | none] ]? )`,
-				},
-			},
-		],
-		"number-max-precision": 12,
-	},
 }
